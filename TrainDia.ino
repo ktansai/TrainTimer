79509eeead6182db18a786085af0342d66e112d4
Keisuke Kawahara <kawahara@whill.jp>
2019-02-24 01:08:18 +0900

    Jsonのレスポンスを読み込みできるようにした。

--- TrainDia.ino
+++ TrainDia.ino
@@ -1,11 +1,13 @@
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <M5Stack.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include "WiFiConfig.h"
 
 
-const char* url = "http://s3-ap-northeast-1.amazonaws.com/ktansai/TrainDia/Tsurumiono_weekday.csv";
+// const char* url = "http://s3-ap-northeast-1.amazonaws.com/ktansai/TrainDia/Tsurumiono_weekday.csv";
+const char* url = "https://f568o9ukoc.execute-api.us-east-1.amazonaws.com/default/trainDiaLambda";
 
 void setup()
 {
@@ -46,6 +48,11 @@ void loop()
         String body = http.getString();
         Serial.print("Response Body: ");
         Serial.println(body);
+        
+        StaticJsonBuffer<200> jsonBuffer;
+        JsonObject& root = jsonBuffer.parseObject(body);
+        const char* time1 = root["train"][0];
+        Serial.println(time1);
     }
     delay(5000);
 }
