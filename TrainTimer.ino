50e0e2e015945a1e0d980346f47fb9d7d500970f
Keisuke Kawahara <kawahara@whill.jp>
2019-08-10 15:57:39 +0900

    Update for ArduinoJson 5 to 6

--- TrainTimer.ino
+++ TrainTimer.ino
@@ -45,13 +45,14 @@ class TrainTimer{
                 Serial.print("Response Body: ");
                 Serial.println(body);
                 
-                StaticJsonBuffer<200> jsonBuffer;
-                JsonObject& root = jsonBuffer.parseObject(body);
+                StaticJsonDocument<64> jsonObj;
+                // JsonObject& root = jsonBuffer.parseObject(body);
+                deserializeJson(jsonObj,body);
 
-                size_of_train_info = root["train"].size();
+                size_of_train_info = jsonObj["train"].size();
 
-                for(int i = 0; i < root["train"].size(); i++){
-                    const char* time1 = root["train"][i];
+                for(int i = 0; i < jsonObj["train"].size(); i++){
+                    const char* time1 = jsonObj["train"][i];
                     int hours, minutes ;
                     sscanf(time1, "%2d:%2d", &hours, &minutes);
                     timeinfo[i].tm_hour = hours;
